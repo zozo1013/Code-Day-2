c41d001cfb02c854e92b5a178b4aa6d588c6793a
Zoe Punjwalni <zoepunjwani@gmail.com>
2019-06-18 16:48:48 -0400

    Day 2: KWK File 2

---
+++ Contents.swift
@@ -0,0 +1,76 @@
+//Day 2 Coding Challenge:
+//Make a function that takes in a number (called num), and multiplies it by 2.
+//The function should print out the result of the number AND
+//the function should print out the original user input.
+//Things to think about: What data type do you need? It should work for decimals—Google around for the data type you need!
+//Extension: Use flow-control: if the number is bigger than 10, divide it by 2. If the number is smaller than 10, mulitply the number by -1
+//Write your function here!
+
+//func number (result :Int){
+//    var a = Int()
+//    var b = 2
+//    print(a * b)
+//}
+//
+//
+//if (Int) <= 0 {
+//   print (a * b)
+//}
+
+//func number (result :Int) {
+//    let a: Double = 2
+//    let b; <= 2
+//    let c = a * b
+//    print;  a * b
+////}
+//func testNum(num: Double) {
+//    if num > 10 {
+//        print(num/2)
+//    }  else if num < 10 {
+//            print(num * 2)
+//    }
+//}
+//    testNum(num: 20.5)
+
+////FOR IN LOOP ARRAY
+//var sponsors = ["adidas", "este", "carolina", "apple", "wework"]
+//
+//for sponsor in sponsors {
+//print ("Shout out to \(sponsor) for help making KWK happen")
+//}
+//
+////FOR IN LOOP DICTIONARY
+//
+//var capitals = ["France": "Paris", "Cuba": "Havana", "Japan": "Tokyo"]
+//for pair in capitals{
+//    print (pair)
+//}
+
+//PRACTICE ARRAY
+//var names = ["mimi", "heidi", "yara", "ava", "becca"]
+//for name in names {
+//    print ("hello \(name)")
+//}
+//
+////say you had a fight, make for in loop update ur array to contain all new friends
+//var newnames = ["karlee", "emma"]
+//for newname in newnames {
+//    print ("hello \(newname)")
+//}
+
+//PRACTICE DICTIONARY
+//var myLocations = ["New York" : 1279.2, "London" : 4426, "Dubai" : 7824]
+//for cities in myLocations {
+//    print("You are currently \(cities.value) miles away from \(cities.key)")
+//}
+
+//LOOPS WITHOUT COLLECTIONS
+
+//var animals = ["red panda", "penguin", "polar bear"]
+//for index in 0..<animals.count {
+//    print("I love " + animals[index])
+//}
+
+//CODE CHALLANGE: Create a function that takes in an array, and  using the... approach, loop through the entire array to print out each element in the array without using in
+
+
